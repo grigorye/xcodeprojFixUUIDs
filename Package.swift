a1f979c5b5d732654fe46e7cef291dcdf7a469d3
Grigory Entin <grigory.entin@gmail.com>
2020-03-09 09:10:08 +0100

    Added definition for .executable product.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "xcodeprojFixUUIDs",
+    products: [
+        .executable(name: "xcodeprojFixUUIDs", targets: ["xcodeprojFixUUIDs"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
